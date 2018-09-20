e1c3b248bfb529a72bdf4031c86c48aeccb65101
ththlisa <ththlisa@gmail.com>
2018-09-20 01:57:51 -0700

    sorted

--- merge_overlap.js
+++ merge_overlap.js
@@ -13,16 +13,11 @@
 
 
 /**
- * Definition for an interval.
- * function Interval(start, end) {
- *     this.start = start;
- *     this.end = end;
- * }
- */
-/**
  * @param {Interval[]} intervals
  * @return {Interval[]}
  */
+var intervals = [[5, 8], [3, 6]];
+intervals.sort(function (a, b) { return a[0] - b[0] });
 
 var merge = function (intervals) {
 
@@ -30,14 +25,17 @@ var merge = function (intervals) {
     var current;
     if (intervals.length <= 1 || intervals == null)
         return intervals;
-    intervals.sort();
+
+
     intervals.forEach(function (i) {
-        //
+        // check if the current element disjoint with i element
+        // if they are seperate, push current array to the result
+        // if they are overlaping, go next
         if (!current || current[1] < i[0]) {
             current = i;
             result.push(current);
         }
-
+        // cuz they are overlap, take the larger END point as the result END point
         else if (current[1] < i[1])
             current[1] = i[1];
     });
@@ -46,5 +44,12 @@ var merge = function (intervals) {
 
 
 };
-var intervals = [[1, 3], [0, 4]];
+
 console.log(merge(intervals));
+
+// my note: By default, the sort() function sorts values as strings.
+// please use the compare function to make sure the numbers are sorted.
+// compare function:
+// var points = [40, 100, 1, 5, 25, 10];
+// points.sort(function (a, b) { return a - b });
+// console.log(points);
